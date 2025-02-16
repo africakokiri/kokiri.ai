f058d3e7ccaf2ae487957660c098b02690f60273
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-16 13:23:07 +0900

    Legacy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { AiModelsCarousel } from "@/components/ai-models-carousel";
 import { Textarea } from "@/components/textarea";
 
 export default function page() {
   return (
     <div className="p-4">
+      <AiModelsCarousel />
       <Textarea />
     </div>
   );
