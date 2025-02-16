7621efba565e241c6074f8ae0ae3887c4f057cf7
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-16 14:34:38 +0900

    레이아웃을 구성하기 위해 AiModelsList 컴포넌트 bg-color 적용

--- src/components/ai-models-list.tsx
+++ src/components/ai-models-list.tsx
@@ -11,7 +11,7 @@ export const AiModelsList = () => {
 
   return (
     <header className="fixed">
-      <ul className="flex flex-col items-center gap-4 p-2">
+      <ul className="flex flex-col items-center gap-2 bg-green-100 p-2">
         {aiModels.map(({ id, name, imageSrc }) => {
           return (
             <li key={id}>
