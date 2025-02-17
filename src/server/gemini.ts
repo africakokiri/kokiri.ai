623e350c1d7fe6698a2be48b184bd7d1f6afbe61
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-17 12:07:43 +0900

    Gemini 서버 함수 추가

---
+++ src/server/gemini.ts
@@ -0,0 +1,15 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+export const Gemini = async (prompt: string) => {
+  const chatSession = model.startChat({
+    history: []
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+
+  return result.response.text();
+};
